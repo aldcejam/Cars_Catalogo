9ebc6c2f7f031a0d1b59094f7b674ef68dafa93e
aldcejam <aldcejamj923@gmail.com>
2023-01-28 08:58:00 -0300

    configs

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,15 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json())
+
+app.get('/', (req, res)=>{
+    return res.json({ola: "ofla"})
+})
+
+app.get("/courses", (req, res)=>{
+    const { name } = req.body
+    return res.json({name})
+})
+
 app.listen(3333)                
